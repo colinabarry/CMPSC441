77024d7d68011ad9319c9d8f6b06c88ddf975d4b
Colin Barry <colin.andrew.barry@gmail.com>
2024-01-24 23:18:42 -0500

    lab2 complete

--- src/lab2/cities_n_routes.py
+++ src/lab2/cities_n_routes.py
@@ -1,46 +1,54 @@
-''' 
+""" 
 Lab 2: Cities and Routes
 
 In the final project, you will need a bunch of cities spread across a map. Here you 
 will generate a bunch of cities and all possible routes between them.
-'''
+"""
 import random
 import itertools
 
-def get_randomly_spread_cities(size, n_cities):
+
+def get_randomly_spread_cities(
+    size: tuple[int, int], n_cities: int
+) -> list[tuple[int, int]]:
     """
-    > This function takes in the size of the map and the number of cities to be generated 
+    > This function takes in the size of the map and the number of cities to be generated
     and returns a list of cities with their x and y coordinates. The cities are randomly spread
     across the map.
-    
+
     :param size: the size of the map as a tuple of 2 integers
     :param n_cities: The number of cities to generate
     :return: A list of tuples, each representing a city, with random x and y coordinates.
     """
     # Consider the condition where x size and y size are different
-    pass
+    return [
+        (random.randint(0, size[0]), random.randint(0, size[1]))
+        for _ in range(n_cities)
+    ]
 
-def get_routes(city_names):
+
+def get_routes(city_names: list[str]):
     """
-    It takes a list of cities and returns a list of all possible routes between those cities. 
-    Equivalently, all possible routes is just all the possible pairs of the cities. 
-    
+    It takes a list of cities and returns a list of all possible routes between those cities.
+    Equivalently, all possible routes is just all the possible pairs of the cities.
+
     :param cities: a list of city names
-    :return: A list of tuples representing all possible links between cities/ pairs of cities, 
-            each item in the list (a link) represents a route between two cities.
+    :return: A list of tuples representing all possible links between cities/ pairs of cities,
+                                    each item in the list (a link) represents a route between two cities.
     """
-    pass
+    return itertools.combinations(city_names, 2)
 
 
 # TODO: Fix variable names
-if __name__ == '__main__':
-    city_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-    '''print the cities and routes'''
+if __name__ == "__main__":
+    city_names = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
+    """print the cities and routes"""
     cities = get_randomly_spread_cities((100, 200), len(city_names))
     routes = get_routes(city_names)
-    print('Cities:')
+    print("Cities:")
     for i, city in enumerate(cities):
-        print(f'{city_names[i]}: {city}')
-    print('Routes:')
+        print(f"{city_names[i]}: {city}")
+    print("Routes:")
     for i, route in enumerate(routes):
-        print(f'{i}: {route[0]} to {route[1]}')
+        print(f"{i}: {route[0]} to {route[1]}")
--- src/lab2/map_cell.py
+++ src/lab2/map_cell.py
@@ -1,4 +1,4 @@
-'''
+"""
 Extra Credit Task-
 
 Tic tac toe input
@@ -22,4 +22,4 @@ Imagine if your user enters "C1" and you need to see if there's an X or O in tha
 Your task is to write a function that can translate from strings of length 2 to a tuple (row, column). Name your function get_row_col; it should take a single parameter which is a string of length 2 consisting of an uppercase letter and a digit.
 
 For example, calling get_row_col("A3") should return the tuple (2, 0) because A3 corresponds to the row at index 2 and column at index 0in the board.
-'''
+"""
