4c3cad867e956f2a2c55ccb3b4998cf3a0a64237
Colin Barry <colin.andrew.barry@gmail.com>
2024-02-27 16:50:49 -0500

    basic perlin noise

--- src/lab5/landscape.py
+++ src/lab5/landscape.py
@@ -2,28 +2,43 @@ import matplotlib.pyplot as plt
 from perlin_noise import PerlinNoise
 import numpy as np
 
+
 def get_elevation(size):
     xpix, ypix = size
-    elevation = np.array([])
-    '''Play around with perlin noise to get a better looking landscape (This is required for the lab)'''
 
-    return elevation
+    """Play around with perlin noise to get a better looking landscape (This is required for the lab)"""
+
+    noise = PerlinNoise(octaves=10, seed=1)
+    elevation = []
+    for i in range(xpix):
+        row = []
+        for j in range(ypix):
+            row.append(noise([i / xpix, j / ypix]))
+        elevation.append(row)
+
+    return np.array(elevation, dtype=float)
+
 
 def elevation_to_rgba(elevation):
     xpix, ypix = np.array(elevation).shape
-    colormap = plt.cm.get_cmap('gist_earth')
-    elevation = (elevation - elevation.min())/(elevation.max()-elevation.min())
-    ''' You can play around with colormap to get a landscape of your preference if you want '''
-    landscape = np.array([colormap(elevation[i, j])[0:3] for i in range(xpix) for j in range(ypix)]).reshape(xpix, ypix, 3)*255
-    landscape = landscape.astype('uint8')
+    colormap = plt.cm.get_cmap("gist_earth")
+    elevation = (elevation - elevation.min()) / (elevation.max() - elevation.min())
+    """ You can play around with colormap to get a landscape of your preference if you want """
+    landscape = (
+        np.array(
+            [colormap(elevation[i, j])[0:3] for i in range(xpix) for j in range(ypix)]
+        ).reshape(xpix, ypix, 3)
+        * 255
+    )
+    landscape = landscape.astype("uint8")
     return landscape
- 
+
 
 get_landscape = lambda size: elevation_to_rgba(get_elevation(size))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     size = 640, 480
     pic = elevation_to_rgba(get_elevation(size))
-    plt.imshow(pic, cmap='gist_earth')
+    plt.imshow(pic, cmap="gist_earth")
     plt.show()
