d7ae60c3e4043fd8204deea9443bba137edf2959
Colin Barry <colin.andrew.barry@gmail.com>
2024-04-25 20:30:14 -0400

    test commit

--- src/lab12/episode.py
+++ src/lab12/episode.py
@@ -7,7 +7,7 @@ As per RL conventions, the function should return a list of tuples
 of the form (observation/state, action, reward) for each turn in the episode.
 Note that observation/state is a tuple of the form (player1_health, player2_health).
 Action is simply the weapon selected by the player.  
-Reward is the reward for the player for that turn.
+Reward is the reward for the player for that turn .
 """
 
 import sys
